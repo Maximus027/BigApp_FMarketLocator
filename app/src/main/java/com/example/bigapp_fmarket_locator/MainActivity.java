072e142c53dfa69fdb8ac30ab7704a0c372134ec
MinaMeltem <meltem@gmail.com>
2017-02-03 00:30:50 -0500

    Design completed

--- app/src/main/java/com/example/bigapp_fmarket_locator/MainActivity.java
+++ app/src/main/java/com/example/bigapp_fmarket_locator/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.bigapp_fmarket_locator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_icon, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
