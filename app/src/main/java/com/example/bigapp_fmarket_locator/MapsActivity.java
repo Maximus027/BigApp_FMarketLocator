21bc4cf56c3cb8940ced14c035164e1b4b6d9a01
SharmaYojana <joy.dfm@gmail.com>
2017-02-03 20:21:37 -0500

    working map

--- app/src/main/java/com/example/bigapp_fmarket_locator/MapsActivity.java
+++ app/src/main/java/com/example/bigapp_fmarket_locator/MapsActivity.java
@@ -1,14 +1,22 @@
 package com.example.bigapp_fmarket_locator;
 
-import android.support.v4.app.FragmentActivity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -27,9 +35,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        gotToLocationZoom(40.739194, -73.930890, 15);
 
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+public void gotToLocation(double lat, double lng){
+        LatLng ll = new LatLng(lat, lng);
+      CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+    mMap.moveCamera(update);
+
+    }
+    private void gotToLocationZoom(double lat, double lng, float zoom){
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mMap.moveCamera(update);
+    }
+
+    public void geoLocate(View view) throws IOException {
+        EditText et = (EditText) findViewById(R.id.edit_text);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address address = list.get(0);
+        String locality = address.getLocality();
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG). show();
+
+        double lat = address.getLatitude();
+        double lng  = address.getLongitude();
+        gotToLocationZoom(lat, lng, 15);
     }
 }
