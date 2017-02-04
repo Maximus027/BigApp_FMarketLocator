525a59d90681c4c9149d3ca2b5f8d08162826226
SharmaYojana <joy.dfm@gmail.com>
2017-02-04 14:33:00 -0500

    last changes

--- app/src/main/java/com/example/bigapp_fmarket_locator/MapsActivity.java
+++ app/src/main/java/com/example/bigapp_fmarket_locator/MapsActivity.java
@@ -1,13 +1,24 @@
 package com.example.bigapp_fmarket_locator;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -18,9 +29,10 @@ import com.google.android.gms.maps.model.LatLng;
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mgoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,14 +49,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         gotToLocationZoom(40.739194, -73.930890, 15);
 
+        mgoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+        mgoogleApiClient.connect();
     }
-public void gotToLocation(double lat, double lng){
+
+    public void gotToLocation(double lat, double lng) {
         LatLng ll = new LatLng(lat, lng);
-      CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
-    mMap.moveCamera(update);
+        CameraUpdate update = CameraUpdateFactory.newLatLng(ll);
+        mMap.moveCamera(update);
 
     }
-    private void gotToLocationZoom(double lat, double lng, float zoom){
+
+    private void gotToLocationZoom(double lat, double lng, float zoom) {
         LatLng ll = new LatLng(lat, lng);
         CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
         mMap.moveCamera(update);
@@ -59,10 +79,53 @@ public void gotToLocation(double lat, double lng){
         Address address = list.get(0);
         String locality = address.getLocality();
 
-        Toast.makeText(this, locality, Toast.LENGTH_LONG). show();
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
 
         double lat = address.getLatitude();
-        double lng  = address.getLongitude();
+        double lng = address.getLongitude();
         gotToLocationZoom(lat, lng, 15);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(1000);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mgoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            Toast.makeText(this, "can't get current location", Toast.LENGTH_LONG).show();
+        }else {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+
+        }
+
     }
 }
